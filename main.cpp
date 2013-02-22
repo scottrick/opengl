ec9046d62a0e8e6757a963f24301c0c69fbf964b
scottrick <scottrick49@gmail.com>
2013-02-21 20:05:55 -0600

    Removed non-library stuff

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "GLContext.h"
-
-/* 
-	OpenGL boilerplate setup.  Requires GLEW, GLFW, and SOIL; also uses GLM.
-*/
-int main (int numArgs, char **args) {
-	GLContext context = GLContext();
-	context.create();
-}
